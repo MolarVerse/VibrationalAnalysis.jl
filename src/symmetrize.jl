9b73c4dfca13db1e01e8137eb9cb4eb380452a43
Josef Maria Gallmetzer <josef.gallmetzer@uibk.ac.at>
2023-04-02 22:22:55 +0200

    Mass weighted hessian included

--- src/symmetrize.jl
+++ src/symmetrize.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra
 
-export symmetrize_addition, symmetrize_multiplication
+export symmetrize_addition, symmetrize_multiplication, mass_weighted_hessian
 
 """
     symmetrize_addition(hessian::Matrix{Float64})
@@ -23,4 +23,27 @@ function symmetrize_multiplication(hessian::Matrix{Float64})
     v, V = eigen(_H)
     _H_sym = V * Diagonal(sqrt.(abs.(v))) * V'
     return _H_sym
+end
+
+"""
+    mass_weight_hessian(hessian::Matrix{Float64}, atom_masses::Vector{Float64})
+
+Mass weight a Hessian matrix.
+"""
+
+function mass_weighted_hessian(hessian::String)
+    # Read the hessian
+    hessian = read_hessian(hessian)
+    # Read the rst file
+    hessian = symmetrize_multiplication(hessian)
+    
+    # Create 3N x 3N matrix of masses
+    masses_repeat = repeat(masses, inner=3)
+    # Matrix of masses \sqrt{m_i m_j}
+    masses_matrix = (masses_repeat * masses_repeat').^(1/2)
+
+    # Mass weight the hessian
+    hessian = hessian ./ masses_mat
+
+    return hessian
 end
