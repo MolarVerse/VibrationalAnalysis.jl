72c5a9ae3bb22fad10cf9d7c252f9a3d5a8cfe79
Josef Maria Gallmetzer <josef.gallmetzer@uibk.ac.at>
2023-04-02 22:23:20 +0200

    Created coordinate transformation

--- src/VibrationalAnalysis.jl
+++ src/VibrationalAnalysis.jl
@@ -2,5 +2,6 @@ module VibrationalAnalysis
 
 include("read_files.jl")
 include("symmetrize.jl")
+include("coordinate_transform.jl")
 
 end # module VibrationalAnalysis
---
+++ src/coordinate_transform.jl
@@ -0,0 +1,60 @@
+
+export center_to_com, calculate_inertia_tensor
+
+"""
+    center_to_com(coord::Matrix{Float64}, masses::Vector{Float64})
+
+Translate the coordinates to the center of mass.
+    
+    Parameters
+    ----------
+    coord : Matrix{Float64}
+        The coordinates of the atoms.
+    masses : Vector{Float64}
+        The masses of the atoms.
+    
+    Returns
+    -------
+    coord : Matrix{Float64}
+        The coordinates of the atoms translated to the center of mass.
+
+"""
+
+function center_to_com(coord::Matrix{Float64}, masses::Vector{Float64})
+    # Calculate the center of mass
+    com = sum(coord .* masses, dims=1) / sum(masses)
+    # Translate the coordinates to the center of mass
+    coord = coord .- com
+    return coord
+end
+
+"""
+    calculate_inertia_tensor(coord::Matrix{Float64}, masses::Vector{Float64})
+
+Calculate the inertia tensor.
+    
+        Parameters
+        ----------
+        coord : Matrix{Float64}
+            The coordinates of the atoms.
+        masses : Vector{Float64}
+            The masses of the atoms.
+    
+        Returns
+        -------
+        inertia_tensor : Matrix{Float64}
+            The inertia tensor.
+    
+    """
+
+function calculate_inertia_tensor(coord::Matrix{Float64}, masses::Vector{Float64})
+    
+    # Initialize the inertia tensor
+    inertia_tensor = zeros(3, 3)
+    # 
+    for i in 1:size(coord, 1)
+        inertia_tensor += masses[i] * (coord[i, :] * coord[i, :]' - coord[i, :] * coord[i, :])
+    end
+
+    return inertia_tensor
+end
