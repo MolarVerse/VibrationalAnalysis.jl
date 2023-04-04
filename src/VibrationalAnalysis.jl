16e929abd94cf763b6902a62319ae2c50ffad1e3
Josef M. Gallmetzer <Josef.Gallmetzer@uibk.ac.at>
2023-04-04 10:14:51 +0200

    Renamed transformation file

--- src/VibrationalAnalysis.jl
+++ src/VibrationalAnalysis.jl
@@ -3,7 +3,7 @@ module VibrationalAnalysis
 include("read_files.jl")
 include("symmetrize.jl")
 include("coordinate_transform.jl")
-include("transformation_matrix.jl")
+include("transformation.jl")
 include("observables.jl")
 include("write_out.jl")
 
