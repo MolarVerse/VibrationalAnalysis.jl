5fb08b317e3bc303d7a955b59079e02c39a6e190
Josef Maria Gallmetzer <josef.gallmetzer@uibk.ac.at>
2023-10-01 21:01:49 +0200

    Initial tests for transformation.jl

--- src/transformation.jl
+++ src/transformation.jl
@@ -1,5 +1,5 @@
 
-export internal_coordinates
+export internal_coordinates, transformation_matrix, translational_modes, rotational_modes
 
 """
     translational_modes(atom_masses::Vector{Float64})
@@ -10,12 +10,12 @@ export internal_coordinates
 function translational_modes(atom_masses::Vector{Float64})
     # Create translation matrix 3N x 3
     masses_diagonal = [diagm(0 => [i, i, i]) for i in atom_masses]
-    
+
     # Convert to Matrix{Float64}
     translation = hcat(masses_diagonal...)'
 
     # Normalize the translation matrix
-    translation = translation ./ sqrt.(sum(translation.^2, dims=1))
+    translation = translation ./ sqrt.(sum(translation .^ 2, dims=1))
 
     return translation
 end
@@ -27,28 +27,29 @@ Calculate the rotational modes of a molecule.
 """
 
 function rotational_modes(atom_coords::Matrix{Float64}, atom_masses::Vector{Float64})
+
     # Translate the coordinates to the center of mass
     atom_coords = center_to_com(atom_coords, atom_masses)
     # Calculate the inertia tensor
     inertia_tens = inertia_tensor(atom_coords, atom_masses)
     # Calculate the eigenvalues and eigenvectors of the inertia tensor
-    _ , eigenvectors = eigen(inertia_tens)
-    
+    _, eigenvectors = eigen(inertia_tens)
+
     # Notation from Gaussian Vibrational Analysis
     X = eigenvectors
     # Calculate the rotational frame
     P = atom_coords * eigenvectors
-    
+
     # Initialize the rotation matrix 3N x 3
     rotation = zeros(size(atom_coords, 1) * 3, 3)
 
     # Calculate the rotation matrix
-    rotation[:, 1] += ((P[:,2] * X[3, :]' .- P[:,3] * X[2,:]') .* sqrt.(atom_masses))[:]
-    rotation[:, 2] += ((P[:,3] * X[1, :]' .- P[:,1] * X[3,:]') .* sqrt.(atom_masses))[:]
-    rotation[:, 3] += ((P[:,1] * X[2, :]' .- P[:,2] * X[1,:]') .* sqrt.(atom_masses))[:]
-    
+    rotation[:, 1] += ((P[:, 2]*X[3, :]'.-P[:, 3]*X[2, :]').*sqrt.(atom_masses))[:]
+    rotation[:, 2] += ((P[:, 3]*X[1, :]'.-P[:, 1]*X[3, :]').*sqrt.(atom_masses))[:]
+    rotation[:, 3] += ((P[:, 1]*X[2, :]'.-P[:, 2]*X[1, :]').*sqrt.(atom_masses))[:]
+
     # Normalize the rotation matrix
-    rotation = rotation ./ sqrt.(sum(rotation.^2, dims=1))
+    rotation = rotation ./ sqrt.(sum(rotation .^ 2, dims=1))
     return rotation
 end
 
@@ -59,15 +60,15 @@ end
 """
 
 function transformation_matrix(atom_coords::Matrix{Float64}, atom_masses::Vector{Float64})
-    
+
     # Initialize transformation matrix 3N x 3N
     transformation = zeros(size(atom_coords, 1) * 3, size(atom_coords, 1) * 3)
-    
+
     # Calculate the translational modes
     translation = translational_modes(atom_masses)
     # Calculate the rotational modes
     rotation = rotational_modes(atom_coords, atom_masses)
-    
+
     # Combine the translational and rotational modes
     transformation[:, 1:3] = translation
     transformation[:, 4:6] = rotation
@@ -92,12 +93,12 @@ function internal_coordinates(atom_coords::Matrix{Float64}, atom_masses::Vector{
     eigenvalues, eigenvectors = eigen(internal_hessian)
 
     # Calculate the masses matrix
-    masses_matrix = diagm(0 => [1/sqrt(i) for i in atom_masses for j in 1:3])
+    masses_matrix = diagm(0 => [1 / sqrt(i) for i in atom_masses for j in 1:3])
     # Calculate the eigenvectors in internal coordinates
     eigenvectors_internal = masses_matrix * transformation * eigenvectors
 
     # Normalize Vectors
-    normalization = sqrt.(1 ./ sum(eigenvectors_internal.^2, dims=1))
+    normalization = sqrt.(1 ./ sum(eigenvectors_internal .^ 2, dims=1))
     # Normalize the eigenvectors
     eigenvectors_internal_normalized = eigenvectors_internal .* normalization
 
--- test/transformation.jl
+++ test/transformation.jl
@@ -1,2 +1,27 @@
 using VibrationalAnalysis
 using Test
+
+@testset "Translation Modes" begin
+    atom_masses = [1.0]
+    translation = translational_modes(atom_masses)
+    @test size(translation) == (3, 3)
+    @test translation == [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+end
+
+@testset "Rotational Modes" begin
+    atom_coords = [0.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0]
+    atom_masses = [1.0, 1.0, 1.0]
+    rotation = rotational_modes(atom_coords, atom_masses)
+    @test size(rotation) == (9, 3)
+    @test rotation == 
+                    [  -0.5773502691896257 0.0 0.0; 
+                        0.28867513459481303 -0.5 -0.43301270189221935; 
+                        0.28867513459481303 0.5 0.43301270189221935; 
+                       -0.5773502691896257 0.0 0.0; 
+                        0.28867513459481303 -0.5 0.43301270189221935; 
+                        0.28867513459481303 0.5 -0.43301270189221935; 
+                        0.0 0.0 0.40824829046386296; 
+                        0.0 0.0 -0.20412414523193156; 
+                        0.0 0.0 -0.20412414523193156
+                    ]
+end
