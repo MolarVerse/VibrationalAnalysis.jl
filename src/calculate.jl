d30f1db22fed613c524f31c2ad121f56d70cff30
Josef M. Gallmetzer <Josef.Gallmetzer@uibk.ac.at>
2023-10-09 10:43:58 +0200

    Write-out fixed

--- src/calculate.jl
+++ src/calculate.jl
@@ -22,8 +22,6 @@ function calculate(rst_file::String, hessian_file::String, moldescriptor_file::S
     reduced_masses = reduced_mass(normalization)
     intensities = infrared_intensity(eigenvectors_internal_normalized, atom_charges, reduced_masses)
     force_constants = force_constant(omega, reduced_masses)
- 
-    print(force_constants)
 
     # Write the modes as xyz-files
     write_modes(eigenvectors_internal_normalized, atom_coords, atom_names)
