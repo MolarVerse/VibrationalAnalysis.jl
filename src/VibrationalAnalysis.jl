ca40ba7316976a7862837ffd117d0e2ccc72afe7
Josef M. Gallmetzer <Josef.Gallmetzer@uibk.ac.at>
2024-02-12 11:42:29 +0100

    Added some more documentation

--- src/VibrationalAnalysis.jl
+++ src/VibrationalAnalysis.jl
@@ -2,6 +2,21 @@
 # VibrationalAnalysis.jl 
 
 This module contains functions to perform vibrational analysis on a QMCFC output.
+
+## Usage
+
+Can read directly from rst-file, moldescriptor-file and hessian-file and perform vibrational analysis on the system.
+
+```julia-repl
+julia> read_calculate("restart.rst", "hessian.dat", "moldescriptor.dat")
+```
+
+Or directly perform a vibrational analysis with atom masses, atom coordinates, atom charges and hessian of the system.
+
+```julia-repl
+julia> read_calculate(atom_masses, atom_coords, atom_charges, hessian)
+```
+
 """
 module VibrationalAnalysis
 
