e01ddbd64707d57cd62ec738aa93a26ab1a3764c
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-07-24 18:37:35 +0200

    refactor: Add test cases for H2O and MeOH normal modes

--- test/calculate.jl
+++ test/calculate.jl
@@ -17,3 +17,19 @@ end
 	@test force_constants ≈ [0.0, 0.0, 0.0, 0.0, 0.0010716286, 0.0015643, 1.4173086, 8.31255, 9.171054] atol = 1e-3
 	@test reduced_masses ≈ [6.00527, 6.005463, 6.0052792, 1.05927, 1.04103, 1.0686666, 1.079864, 1.0480, 1.0868] atol = 1e-4
 end
+
+@testset "Normal Modes - H2O" begin
+	atom_names, atom_masses, atom_coords, atom_types = read_rst("data/h2o_compare/final_h2o.xyz.rst")
+	hessian = read_hessian("data/h2o_compare/hessian-5p-0.001.dat")
+	_, _, _, eigenvectors_internal_normalized = calculate(atom_masses, atom_coords, hessian)
+	test_modes = hcat(map(row -> parse.(Float64, row), split.(readlines("data/h2o_compare/normal_modes_h2o.dat")))...)
+	@test eigenvectors_internal_normalized ≈ test_modes atol = 1e-5
+end
+
+@testset "Normal Modes - MeOH" begin
+	atom_names, atom_masses, atom_coords, atom_types = read_rst("data/meoh_compare/final_meoh.xyz.rst")
+	hessian = read_hessian("data/meoh_compare/hessian.dat")
+	_, _, _, eigenvectors_internal_normalized = calculate(atom_masses, atom_coords, hessian)
+	test_modes = hcat(map(row -> parse.(Float64, row), split.(readlines("data/meoh_compare/meoh_normal_modes.dat")))...)
+	@test eigenvectors_internal_normalized ≈ test_modes atol = 1e-5
+end
